efd677f68f193ec82f1ae6669ab40102b96b9407
Greg Taylor <hesitatetowonder@gmail.com>
2017-01-16 21:08:21 -0700

    add exercise 2

---
+++ GasolineReceipt.java
@@ -0,0 +1,16 @@
+public class GasolineReceipt {
+  public static void main( String[] arg ) {
+    System.out.println( "+------------------------+" );
+    System.out.println( "|                        |" );
+    System.out.println( "|      Corner Store      |" );
+    System.out.println( "|                        |" );
+    System.out.println( "| 2017-01-15     08:01PM |" );
+    System.out.println( "|                        |" );
+    System.out.println( "| Gallons:        10.870 |" );
+    System.out.println( "| Price/gallon:  $ 3.090 |" );
+    System.out.println( "|                        |" );
+    System.out.println( "| Fuel total:    $ 33.59 |" );
+    System.out.println( "|                        |" );
+    System.out.println( "+------------------------+" );
+  }
+}

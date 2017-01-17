89ccbfb155d97d9520807628b4d837cf031b1747
Greg Taylor <hesitatetowonder@gmail.com>
2017-01-16 21:07:39 -0700

    add exercise 1

---
+++ FirstProg.java
@@ -0,0 +1,6 @@
+public class FirstProg {
+   public static void main( String[] args ) {
+     System.out.println( "I, Greg Taylor, am determined to learn how to code." );
+     System.out.println( "Today's date is Sunday, January 15, 2017" );
+   }
+ }

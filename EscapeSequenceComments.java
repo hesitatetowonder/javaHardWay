b8406a9a0b9d9f07ab92fcffc3f00d6eb2ffc228
Greg Taylor <hesitatetowonder@gmail.com>
2017-01-16 21:09:57 -0700

    add exercise 4

---
+++ EscapeSequenceComments.java
@@ -0,0 +1,18 @@
+public class EscapeSequenceComments {
+  public static void main( String[] args ) {
+    // This exercis demonstrates escape sequences & comments (like these)!
+    System.out.print( "Learm\tJava\n\tthe\nHard\tWay\n\n" );
+    System.out.print( "\tLearn Java the \"Hard\" Way!\n" );
+    // System.out.frimp( "Learn Java the Hard Way" );
+    System.out.print( "Hello\n" ); // This line prints Hello.
+    System.out.print( "Jello\by\n" ); // This line prints Jelly.
+    /* The quick brown fox jumped over a lazy dog.
+       Quick wafting zephyrs vex bold Jim. */
+    System.out /* testing */ .println( "Hard to believe, eh?" );
+    System.out.println( "Surprised? /* abcde */ Or what did you expect?" );
+    System.out.println( "\\// -=- \\//" );
+    System.out.println( "\\\\ \\\\\\ \\\\\\\\" ); // it takes 2 to make 1
+    System.out.print( "I hope you understand \"escape sequences\" now.\n" );
+    // and comments. :)
+  }
+} // I think it will compile with line 13 block comment inside println. I was wrong
